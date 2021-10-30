8fa71a1269fc1ba002168e3d5de1c8dda2ae22b2
joohyungsongnbcc <joohyungsong0205@gmail.com>
2021-10-30 09:19:40 -0300

    section3 lecture 41

--- script.js
+++ script.js
@@ -161,7 +161,7 @@ console.log(age1, age2, age3);
 const ages = [calcAge(year[0]), calcAge(year[1]),
   calcAge(year[year.length - 1])];
 console.log(ages);*/
-
+/*
 const friends = ['Michael', 'Steven', 'Peter'];
 
 
@@ -190,4 +190,17 @@ console.log(friends.includes(23));
 
 if (friends.includes('Steven')) {
   console.log('You have a friend called Peter');
+}*/
+
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 :
+  bill * 0.2;
+
 }
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]),calcTip(bills[2])];
+
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+
+
+console.log(bills, tips, totals);
