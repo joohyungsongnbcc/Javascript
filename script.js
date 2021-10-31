70a78b5e7966d859ce47cdeecb4919cced38a5fe
joohyungsongnbcc <joohyungsong0205@gmail.com>
2021-10-31 19:38:13 -0300

    section 3 lecture 49

--- script.js
+++ script.js
@@ -371,7 +371,7 @@ for (let i = 0; i < jonas.length; i++) {
   console.log(jonas[i], typeof jonas[i]);
 }
 */
-
+/*
 const jonas = [
   'Jonas',
   'Schmedtmann',
@@ -391,4 +391,26 @@ for (let exercise = 1; exercise < 4; exercise++) {
   for (let rep = 1; rep < 6; rep++) {
     console.log(`Exercise ${exercise}: Lifting weight repetition ${rep}🏋️‍♂️`);
   }
+}*/
+
+for (let rep = 1; rep <= 10; rep++) {
+  console.log(`Listing weights repetition ${rep}🏋️‍♂️ `);
 }
+
+let rep = 1;
+while (rep <= 10) {
+
+  //console.log(`WHILE: Listing weights repetition ${rep}🏋️‍♂️ `);
+  rep++;
+}
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+
+
+while (dice !== 6) {
+  console.log(`You rolled a ${dice}`);
+  dice = Math.trunc(Math.random() * 6) + 1;
+  if (dice === 6) console.log('Loop is about to end...');
+}
+
+
