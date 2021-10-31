7795f081679cc933c914d9206108db1a2d30ddcb
joohyungsongnbcc <joohyungsong0205@gmail.com>
2021-10-31 19:54:35 -0300

    section 3 coding challenge #3

--- script.js
+++ script.js
@@ -392,7 +392,7 @@ for (let exercise = 1; exercise < 4; exercise++) {
     console.log(`Exercise ${exercise}: Lifting weight repetition ${rep}🏋️‍♂️`);
   }
 }*/
-
+/*
 for (let rep = 1; rep <= 10; rep++) {
   console.log(`Listing weights repetition ${rep}🏋️‍♂️ `);
 }
@@ -412,5 +412,35 @@ while (dice !== 6) {
   dice = Math.trunc(Math.random() * 6) + 1;
   if (dice === 6) console.log('Loop is about to end...');
 }
+*/
 
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 :
+    bill * 0.2;
+}
 
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100,
+86, 52];
+const tips = [];
+const totals = [];
+
+for (let i = 0; i < bills.length; i++) {
+  const tip = calcTip(bills[i]);
+  tips.push(tip);
+  totals.push(tip + bills[i]);
+}
+
+console.log(bills, tips, totals);
+
+const calcAverage = function (arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    //sum = sum + arr[i];
+    sum += arr[i];
+  
+}
+  return sum / arr.length;
+}
+console.log(calcAverage([2, 3, 7]));
+console.log(calcAverage(totals));
+console.log(calcAverage(tips));
